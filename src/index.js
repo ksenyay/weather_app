f9e8155fdfed37cfb55ce43c6c408f110b80af54
ksenyay <ksenyausachova@gmail.com>
2025-02-03 13:20:29 +0200

    Added current weather block with changing values according to the input

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import "./styles.css";
 const form = document.querySelector("#location_form");
 const input = document.querySelector("#location");
 
+// CURRENT WEATHER
+
+const currentCity = document.querySelector("#city");
+const currentCountry = document.querySelector("#country");
+const currentTemperature = document.querySelector("#temperature");
+const currentCondition = document.querySelector("#condition");
+const feelsLike = document.querySelector("#feels_like");
+
+// END OF CURRENT WEATHER
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const location = input.value.trim();
@@ -21,13 +31,31 @@ async function getCurrentWeather(location) {
     if (!response.ok) {
       throw new Error(`Error: ${response.status} ${response.statusText}`);
     }
-
     const responseData = await response.json();
-    console.log(responseData.current.humidity);
+    console.log(responseData);
+    setLocation(responseData.location.name, responseData.location.country);
+    setTemperature(responseData.current.temp_c);
+    setCondition(responseData.current.condition.text);
+    setFeelsLike(responseData.current.feelslike_c);
   } catch (error) {
     console.error("Failed to fetch weather data:", error.message);
     alert("Please enter a valid value!");
   }
 }
 
-//function setLocation(location) {}
+function setLocation(city, country) {
+  currentCity.textContent = `${city}, `;
+  currentCountry.textContent = country;
+}
+
+function setTemperature(temp) {
+  currentTemperature.textContent = `${Math.round(temp)}`;
+}
+
+function setCondition(condition) {
+  currentCondition.textContent = condition;
+}
+
+function setFeelsLike(feelsTemp) {
+  feelsLike.textContent = `Feels like ${Math.round(feelsTemp)} °C`;
+}
