0d324beb8f466166d04d128cbd07f4853c6dd001
ksenyay <ksenyausachova@gmail.com>
2025-02-09 16:55:15 +0200

    Updates

--- 32bddc5559bdb3a30a1b.js
+++ 32bddc5559bdb3a30a1b.js
@@ -14,9 +14,6 @@ function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), 
 function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
 function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 import "./styles.css";
-
-// Make font red
-
 document.body.style.zoom = "110%"; // Adjusting default page zoom
 
 // Hangle display of the data on the page
