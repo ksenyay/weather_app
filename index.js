7a69e75747edd942a0d818c2105d4ed085406ad1
ksenyay <ksenyausachova@gmail.com>
2025-02-09 16:41:59 +0200

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-require("./styles.css?v=1.1");
+require("./styles.css");
 document.body.style.zoom = "110%"; // Adjusting default page zoom
 
 // Hangle display of the data on the page
